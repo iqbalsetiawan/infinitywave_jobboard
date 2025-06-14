772207d382d6af93eb3373db5fdbdf14795cfe20
Iqbal Setiawan <52906490+iqbalsetiawan@users.noreply.github.com>
2025-06-14 15:25:17 +0000

    Improve grid layout of job cards

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -32,7 +32,7 @@ const JobList = () => {
                 </h2>
             </div>
 
-            <div className="grid gap-6">
+            <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
                 {jobs.map((job) => (
                     <JobCard key={job.id} job={job} />
                 ))}
